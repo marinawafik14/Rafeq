29a6ac09074ac44000ba57954879e984ce311eca
dareen3laa <dareen3laa2002@gmail.com>
2025-07-02 00:08:49 +0300

    stripe

--- src/app/mentee/booking-form/booking-form.component.ts
+++ src/app/mentee/booking-form/booking-form.component.ts
@@ -12,7 +12,7 @@ import { Bookings } from '../../Models/Bookings';
 @Component({
   selector: 'app-booking-form',
   standalone: true,
-  imports: [CommonModule, FormsModule, MenteeLayoutComponent],
+  imports: [CommonModule, FormsModule],
   templateUrl: './booking-form.component.html',
   styleUrls: ['./booking-form.component.css']
 })
