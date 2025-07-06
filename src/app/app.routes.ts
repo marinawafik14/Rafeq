1907bf05f8bd00a0b6dba64a1343f3e66732321a
dareen3laa <dareen3laa2002@gmail.com>
2025-07-07 02:09:34 +0300

    design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -35,6 +35,10 @@ import { ArticleDetailComponent } from './Auth/article-detail/article-detail.com
 import { authGuardGuard } from './guards/auth-guard.guard';
 import { AdminDashboardComponent } from './Components/Admin/admin-dashboard/admin-dashboard.component';
 import { NotificationsComponent } from './features/notifications/notifications.component'; // Add this import
+import { AdminArticleListComponent } from './Auth/admin-article-list/admin-article-list.component';
+import { ArticleFormComponent } from './Auth/article-form/article-form.component';
+import { AdminFaqListComponent } from './Auth/admin-faq-list/admin-faq-list.component';
+import { FaqFormComponent } from './Auth/faq-form/faq-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
