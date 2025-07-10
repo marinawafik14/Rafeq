3b82e5a9c0be900e15d6edd12dc8822918517ae4
Rawan-Labeb <rawan.w.labeb@gmail.com>
2025-07-10 23:28:39 +0300

    fix notifcation appearnce , add date filter in booking and hide static meetinglink

--- src/app/Models/Notification/notification.model.ts
+++ src/app/Models/Notification/notification.model.ts
@@ -3,7 +3,7 @@ export interface NotificationDto {
   notificationId: number;
   message: string;
   isRead: boolean;
-  type: 'SessionReminder' | 'NewBooking' | 'NewReview' | 'PaymentConfirmed' | 'BookingCancelled' | 'SystemNotification' | 'NewMessage';
+  type: 'SessionReminder' | 'NewBooking' | 'NewReview' | 'PaymentConfirmed' | 'BookingCancelled' | 'SystemNotification' ;
   relatedEntityId?: number;
   createdAt: Date;
 }
